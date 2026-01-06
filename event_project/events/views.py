9c2c3c7c7ec2159fe137b845c54e2a61db39423f
umeshbogati <bogatiu17@gmail.com>
2026-01-06 20:56:54 +0545

    here I worked views to create home , user register and signup.

--- event_project/events/views.py
+++ event_project/events/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from .models import Event, Registration
+from .forms import EventForm
 
 # Create your views here.
+def home(request):
+    events = Event.objects.all()
+    return render(request, 'events/home.html', {'events': events})
+
+@login_required
+def create_event(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            event =form.save(commit=False)
+            event.created_by = request.user
+            event.save()
+            return redirect('home')
+        else:
+            form = EventForm()
+        return render(request, 'events/create_events.html', {'form': form})
+
+@login_required
+def register_event(request, event_id):
+    event = get_object_or_404(Event, id=event_id)
+    Registration.objects.get_or_create(user=request.user, event=event)
+    return redirect('home')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
