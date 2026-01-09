8420ef285456e92c683fd28a3a6296eba9c1bbc8
umeshbogati <bogatiu17@gmail.com>
2026-01-09 08:35:25 +0545

    Here I work on event_project urls.py where i give path for api.

--- event_project/event_project/urls.py
+++ event_project/event_project/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
@@ -24,4 +25,7 @@ urlpatterns = [
     path('', include('events.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    
+    path('api/', include('events.api_urls')),
+    path('api/login/', obtain_auth_token),
 ]
